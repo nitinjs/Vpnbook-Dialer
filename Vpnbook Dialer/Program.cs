73e410e8d1cd226d3653154bc4e7c646c068ffe2
nitin sawant <nitin.sawant@datamatics.com>
2019-02-07 19:47:52 +0530

    Converting Captcha image to Text

--- Vpnbook Dialer/Program.cs
+++ Vpnbook Dialer/Program.cs
@@ -1,7 +1,9 @@
 ﻿using CsQuery;
+using IronOcr;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,7 +15,7 @@ namespace Vpnbook_Dialer
     {
         static void Main(string[] args)
         {
-            // VPN adapters are stored in the rasphone.pdk
+            // VPN adapters are stored in the rasphone.pdk => Windows7
             // "C:\Users\Me\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk"
             string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) +
                           @"\Microsoft\Network\Connections\Pbk\_hiddenPbk\rasphone.pbk";//test 
@@ -70,11 +72,35 @@ namespace Vpnbook_Dialer
 
         static string GetVpnbookPassword()
         {
+            Console.WriteLine("Retrieving captcha image");
+            string domain = "http://www.vpnbook.com/";
             var scraper = new ScraperApp.ScraperWebClient(false);
-            var strVpnbook = scraper.DownloadString("http://www.vpnbook.com/");
+            var strVpnbook = scraper.DownloadString(domain);
             CQ dom = strVpnbook;
-            var elm = dom.Find("strong:contains('Password:')").First();
-            var password = elm.Text().Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries).Last().Trim();
+
+            var imagePath = domain + dom.Find("strong:contains('Password:')").First().Find("img").First().Attr("src");
+            byte[] bytes = scraper.DownloadData(imagePath);
+            MemoryStream ms = new MemoryStream(bytes);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+
+            Console.WriteLine("Converting to captcha to text");
+            var Ocr = new AdvancedOcr()
+            {
+                CleanBackgroundNoise = true,
+                EnhanceContrast = true,
+                EnhanceResolution = true,
+                Language = IronOcr.Languages.English.OcrLanguagePack,
+                Strategy = IronOcr.AdvancedOcr.OcrStrategy.Advanced,
+                ColorSpace = AdvancedOcr.OcrColorSpace.Color,
+                DetectWhiteTextOnDarkBackgrounds = true,
+                InputImageType = AdvancedOcr.InputTypes.Document,
+                RotateAndStraighten = true,
+                ReadBarCodes = false,
+                ColorDepth = 4
+            };
+            var Result = Ocr.Read(img);
+
+            var password = Result.Text;//elm.Text().Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries).Last().Trim();
 
             return password;
         }
